99c3db96ee267bd7b1a0ff43addffe4f27d860db
Ricky Thomas <rjtom2@hotmail.com>
2024-09-17 15:20:41 -0400

    add files

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+string nickName = "R.J.";
+string firstName = "Ricky";
+string lastName = "Thomas";
+string occupation = "delivery driver at FedEx";
+string futureOccupation = "Software Developer";
+string intro = $"\tHello my name is {firstName} {lastName}!\n\tBut my friends call me {nickName}.\n\tI'm currently a {occupation},\n\tBut will hopefully someday soon be a {futureOccupation}!";
+
+Console.WriteLine(intro);
